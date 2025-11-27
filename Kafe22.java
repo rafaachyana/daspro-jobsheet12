3eb31799434154d7a5c275563f49260023c7a13b
rafaachyana <rafaachyana778@gmail.com>
2025-11-27 08:46:06 +0700

    2

--- Kafe22.java
+++ Kafe22.java
@@ -1,5 +1,5 @@
 public class Kafe22 {
-    public static void Menu(){
+    public static void main(String[]args){
        System.out.println("==== MENU RESTO CAFE ====");
        System.out.println("1. Kopi Hitam - Rp 15,000");
        System.out.println("2. Cappucino - Rp 20,000");
@@ -9,8 +9,5 @@ public class Kafe22 {
        System.out.println("6. Mie Goreng - Rp 18,000");
        System.out.println("============================");
        System.out.println("Silahkan pilih menu yang anda inginkan");
-    }
-    public static void main(String[]args){
-        Menu();
     }
 }

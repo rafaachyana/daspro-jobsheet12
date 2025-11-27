ea6963e7e33dfbe27b373bbc7caef6580422ffc6
rafaachyana <rafaachyana778@gmail.com>
2025-11-27 09:19:01 +0700

    6

--- Kafe22.java
+++ Kafe22.java
@@ -1,20 +1,25 @@
 public class Kafe22 {
-    public static void Menu(String namaPelanggan, boolean isMember){
-        System.out.println("Selamat datang,"+namaPelanggan+"!"); 
-        if (isMember){
-        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
-       }
-       System.out.println("==== MENU RESTO CAFE ====");
-       System.out.println("1. Kopi Hitam - Rp 15,000");
-       System.out.println("2. Cappucino - Rp 20,000");
-       System.out.println("3. Latte -- Rp 22,000");
-       System.out.println("4. Teh Tarik - Rp 12,000");
-       System.out.println("5. Roti Bakar - Rp 10,000");
-       System.out.println("6. Mie Goreng - Rp 18,000");
-       System.out.println("============================");
-       System.out.println("Silahkan pilih menu yang anda inginkan");
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
+        }
+        if (kodePromo.equals("Diskon 50")) {
+            System.out.println("Kode masih berlaku. Anda mendapat diskon 50%");
+        } else if (kodePromo.equals("Diskon 30")) {
+            System.out.println("Kode masih berlaku. Anda mendapat diskon 30%");
+        } 
+        System.out.println("==== MENU RESTO CAFE ====");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("============================");
+        System.out.println("Silahkan pilih menu yang anda inginkan");
     }
-    public static void main(String[]args){
-        Menu("Andi",true);
+    public static void main(String[] args) {
+        Menu("Andi", true, "Diskon 50");
     }
 }
